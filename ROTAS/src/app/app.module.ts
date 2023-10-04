06603ae853a7ab357efde28d1aa591971da76df2
LeonardoJr96 <leo70x7000@gmail.com>
2023-10-04 12:20:32 -0300

    Create shared and add RouterLink

--- ROTAS/src/app/app.module.ts
+++ ROTAS/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 
 import { IndexModule } from './pages/index/index.module';
 import { PortifolioModule } from './pages/portifolio/portifolio.module';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
