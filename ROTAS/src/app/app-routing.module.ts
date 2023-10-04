eae2154d764319ff35ae6f2b52c68847af5f9c76
LeonardoJr96 <leo70x7000@gmail.com>
2023-10-04 12:07:01 -0300

    add joker route

--- ROTAS/src/app/app-routing.module.ts
+++ ROTAS/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { TitleComponent } from './pages/index/title/title.component';
 import { CardComponent } from './pages/portifolio/card/card.component';
 
 const routes: Routes = [
-  {path:'', component: TitleComponent, pathMatch:'full'}, {path:'portfolio', component: CardComponent, pathMatch:'prefix'}
+  {path:'', component: TitleComponent, pathMatch:'full'}, {path:'portfolio', component: CardComponent, pathMatch:'prefix'},
+  {path: '**', redirectTo:''}
 ];
 
 @NgModule({
