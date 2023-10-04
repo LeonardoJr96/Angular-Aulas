7771d4d987267bf11008b4dea7446840c64f99d5
LeonardoJr96 <leo70x7000@gmail.com>
2023-10-04 13:11:54 -0300

    Add Children routes

--- ROTAS/src/app/app-routing.module.ts
+++ ROTAS/src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { TitleComponent } from './pages/index/title/title.component';
 import { CardComponent } from './pages/portifolio/card/card.component';
 
 const routes: Routes = [
-  {path:'', component: TitleComponent, pathMatch:'full'}, {path:'portfolio/:id', component: CardComponent, pathMatch:'prefix'},
+  {path:'', component: TitleComponent, pathMatch:'full'},
+  {path:'portfolio', component: CardComponent, children:[
+    {path:'id', component: CardComponent},
+    {path:'id/:token', component: CardComponent},
+  ]},
   {path: '**', redirectTo:''}
 ];
 
